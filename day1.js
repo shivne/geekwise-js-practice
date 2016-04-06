87fa0aa7668daf18377d96cf0543e4c4183c1444
shivne <mullich@mail.fresnostate.edu>
2016-04-05 21:59:38 -0700

    adding day1.js to repository

---
+++ day1.js
@@ -0,0 +1,4 @@
+var myFavoriteNumber = 5;
+var notMyFavoriteNumber = "15";
+var averageNumber = (myFavoriteNumber / 2) + (notMyFavoriteNumber / 2)
+console.log("My favorite number is: " + myFavoriteNumber + ", it's not: " + notMyFavoriteNumber);
