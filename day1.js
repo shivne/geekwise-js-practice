936dbe80f5bfce53122694bf2191c3c50d8fa8a7
shivne <mullich@mail.fresnostate.edu>
2016-04-05 22:18:47 -0700

    Adding a second commit

--- day1.js
+++ day1.js
@@ -2,3 +2,5 @@ var myFavoriteNumber = 5;
 var notMyFavoriteNumber = "15";
 var averageNumber = (myFavoriteNumber / 2) + (notMyFavoriteNumber / 2)
 console.log("My favorite number is: " + myFavoriteNumber + ", it's not: " + notMyFavoriteNumber);
+
+/*Adding a comment and committing this file a second time.*/
